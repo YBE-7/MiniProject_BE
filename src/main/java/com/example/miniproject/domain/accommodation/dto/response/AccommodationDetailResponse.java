0ce04da6615490a497dfb063f055277759e836d0
KyuCheol Hwang <iambigman97@naver.com>
2023-11-23 03:24:51 +0900

    Feat: 숙소 상세조회 응답 DTO 구현
    AccommodationDetailResponse 구현
    
    Related to: #38

---
+++ src/main/java/com/example/miniproject/domain/accommodation/dto/response/AccommodationDetailResponse.java
@@ -0,0 +1,33 @@
+package com.example.miniproject.domain.accommodation.dto.response;
+
+import com.example.miniproject.domain.accommodation.entity.Accommodation;
+import com.example.miniproject.domain.accommodation.entity.AccommodationImage;
+import java.util.Arrays;
+import java.util.List;
+
+public record AccommodationDetailResponse(
+    String name,
+    Double star,
+    LocationResponse location,
+    String introduction,
+    List<String> services,
+    List<String> images
+) {
+    public AccommodationDetailResponse(
+        Accommodation accommodation
+    ) {
+        this(
+            accommodation.getName(),
+            accommodation.getStar(),
+            new LocationResponse(accommodation.getLocation()),
+            accommodation.getIntroduction(),
+            Arrays.stream(accommodation.getService()
+                .split(","))
+                .toList(),
+            accommodation.getImages()
+                .stream()
+                .map(AccommodationImage::getUrl)
+                .toList()
+        );
+    }
+}
