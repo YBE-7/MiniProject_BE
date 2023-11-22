0374fa7dd59fc78d8bde4edd7a169d1fa63938e0
KyuCheol Hwang <iambigman97@naver.com>
2023-11-22 10:52:51 +0900

    Feat: BaseTimeEntity 구현
    BaseTimeEntity 구현
    
    Related to: #22

---
+++ src/main/java/com/example/miniproject/global/entity/BaseTimeEntity.java
@@ -0,0 +1,19 @@
+package com.example.miniproject.global.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import java.time.LocalDateTime;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+
+    @Column(updatable = false)
+    @CreatedDate
+    private LocalDateTime createdAt;
+}
