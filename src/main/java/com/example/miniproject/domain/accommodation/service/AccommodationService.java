938e554fb3e8e9c65bef9fd0121c2b70cc138380
KyuCheol Hwang <iambigman97@naver.com>
2023-11-22 11:22:49 +0900

    Feat: 숙소 등록 Service 구현
    AccommodationService의 register 메소드 구현
    
    Related to: #24

--- src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationService.java
+++ src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationService.java
@@ -1,5 +1,9 @@
 package com.example.miniproject.domain.accommodation.service;
 
+import com.example.miniproject.domain.accommodation.dto.request.AccommodationRegisterRequest;
+import com.example.miniproject.domain.accommodation.dto.response.AccommodationRegisterResponse;
+
 public interface AccommodationService {
 
+    AccommodationRegisterResponse register(AccommodationRegisterRequest request);
 }
