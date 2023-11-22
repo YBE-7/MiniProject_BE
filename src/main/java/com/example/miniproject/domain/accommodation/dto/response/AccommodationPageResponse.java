2294d0b429776045a00c776696c62e7353924eca
KyuCheol Hwang <iambigman97@naver.com>
2023-11-23 02:50:26 +0900

    Feat: 숙소 조회 응답 DTO 구현
    숙소 조회 응답 DTO 구현
    
    Related to: #28

---
+++ src/main/java/com/example/miniproject/domain/accommodation/dto/response/AccommodationPageResponse.java
@@ -0,0 +1,20 @@
+package com.example.miniproject.domain.accommodation.dto.response;
+
+import java.util.List;
+import org.springframework.data.domain.Page;
+
+public record AccommodationPageResponse(
+    List<AccommodationResponse> results,
+    long totalElements,
+    int totalPages
+) {
+    public AccommodationPageResponse(
+        Page<AccommodationResponse> page
+    ) {
+        this(
+            page.getContent(),
+            page.getTotalElements(),
+            page.getTotalPages()
+        );
+    }
+}
