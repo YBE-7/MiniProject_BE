e0c9bbfd43846e64c89efa1b61c8adee2ddac9a7
KyuCheol Hwang <iambigman97@naver.com>
2023-11-23 02:48:19 +0900

    Feat: 숙소 조회 Service 구현
    AccommodationService의 search 메소드 구현
    
    Related to: #28

--- src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationService.java
+++ src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationService.java
@@ -1,9 +1,13 @@
 package com.example.miniproject.domain.accommodation.service;
 
 import com.example.miniproject.domain.accommodation.dto.request.AccommodationRegisterRequest;
+import com.example.miniproject.domain.accommodation.dto.request.AccommodationSearchCondition;
+import com.example.miniproject.domain.accommodation.dto.response.AccommodationPageResponse;
 import com.example.miniproject.domain.accommodation.dto.response.AccommodationRegisterResponse;
 
 public interface AccommodationService {
 
     AccommodationRegisterResponse register(AccommodationRegisterRequest request);
+
+    AccommodationPageResponse search(AccommodationSearchCondition condition);
 }
--- src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationServiceImpl.java
+++ src/main/java/com/example/miniproject/domain/accommodation/service/AccommodationServiceImpl.java
@@ -1,6 +1,8 @@
 package com.example.miniproject.domain.accommodation.service;
 
 import com.example.miniproject.domain.accommodation.dto.request.AccommodationRegisterRequest;
+import com.example.miniproject.domain.accommodation.dto.request.AccommodationSearchCondition;
+import com.example.miniproject.domain.accommodation.dto.response.AccommodationPageResponse;
 import com.example.miniproject.domain.accommodation.dto.response.AccommodationRegisterResponse;
 import com.example.miniproject.domain.accommodation.entity.Accommodation;
 import com.example.miniproject.domain.accommodation.entity.AccommodationImage;
@@ -20,8 +22,15 @@ public class AccommodationServiceImpl implements AccommodationService {
     public AccommodationRegisterResponse register(AccommodationRegisterRequest request) {
         Accommodation accommodation = request.toEntity();
         request.images()
-            .stream()
             .forEach(image -> accommodation.addImage(AccommodationImage.create(image)));
         return new AccommodationRegisterResponse(accommodationRepository.save(accommodation));
+    }
+
+    @Override
+    @Transactional
+    public AccommodationPageResponse search(AccommodationSearchCondition condition) {
+        return new AccommodationPageResponse(
+            accommodationRepository.findBySearchCondition(condition)
+        );
     }
 }
