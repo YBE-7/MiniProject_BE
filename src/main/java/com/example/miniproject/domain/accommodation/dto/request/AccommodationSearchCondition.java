17515af3af10aae93a14fa529df57950eb3337ed
KyuCheol Hwang <iambigman97@naver.com>
2023-11-22 17:10:21 +0900

    Feat: 숙소 조회 DTO 구현
    숙소 조회 요청, 응답 DTO 구현
    
    Related to: #28

---
+++ src/main/java/com/example/miniproject/domain/accommodation/dto/request/AccommodationSearchCondition.java
@@ -0,0 +1,17 @@
+package com.example.miniproject.domain.accommodation.dto.request;
+
+import com.example.miniproject.global.constant.AccommodationType;
+import com.example.miniproject.global.constant.Region;
+import com.example.miniproject.global.constant.SearchOrderCondition;
+import java.time.LocalDate;
+
+public record AccommodationSearchCondition(
+    AccommodationType type,
+    Region region,
+    LocalDate checkinDate,
+    LocalDate checkoutDate,
+    Integer capacity,
+    SearchOrderCondition order
+) {
+
+}
---
+++ src/main/java/com/example/miniproject/global/constant/SearchOrderCondition.java
@@ -0,0 +1,5 @@
+package com.example.miniproject.global.constant;
+
+public enum SearchOrderCondition {
+    STAR_DESC, PRICE_ASC, PRICE_DESC
+}
