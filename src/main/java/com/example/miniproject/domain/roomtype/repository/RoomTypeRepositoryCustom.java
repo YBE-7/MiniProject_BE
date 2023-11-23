abd9856975bd6f1d88602d8ce65a77b6d4c47db8
KyuCheol Hwang <iambigman97@naver.com>
2023-11-23 12:00:47 +0900

    Feat: 주문할 객실 반환 Repository 구현
    RoomTypeRepository의 가능한 객실 반환 기능 구현
    
    Related to: #41

--- src/main/java/com/example/miniproject/domain/roomtype/repository/RoomTypeRepositoryCustom.java
+++ src/main/java/com/example/miniproject/domain/roomtype/repository/RoomTypeRepositoryCustom.java
@@ -1,10 +1,18 @@
 package com.example.miniproject.domain.roomtype.repository;
 
+import com.example.miniproject.domain.room.entity.Room;
 import com.example.miniproject.domain.roomtype.entity.RoomType;
 import java.time.LocalDate;
+import java.util.List;
 
 public interface RoomTypeRepositoryCustom {
     Long findStockBySchedule(
+        RoomType roomType,
+        LocalDate checkinDate,
+        LocalDate checkoutDate
+    );
+
+    List<Room> findAvailableRooms(
         RoomType roomType,
         LocalDate checkinDate,
         LocalDate checkoutDate
