6f51affc92c457a28383fcb74566d557a4e758ee
KyuCheol Hwang <iambigman97@naver.com>
2023-11-24 21:29:07 +0900

    Refactor: 숙소 검색 응답 DTO 변경
    숙소 검색 응답 DTO 변경
    
    Related to: #66

--- src/main/java/com/example/miniproject/domain/accommodation/dto/response/AccommodationPageResponse.java
+++ src/main/java/com/example/miniproject/domain/accommodation/dto/response/AccommodationPageResponse.java
@@ -4,7 +4,7 @@ import java.util.List;
 import org.springframework.data.domain.Page;
 
 public record AccommodationPageResponse(
-    List<AccommodationResponse> results,
+    List<AccommodationResponse> accommodations,
     long totalElements,
     int totalPages
 ) {
