fe0ef2b61d22ee6bbe529c1ee752a73ba91f5e8b
KyuCheol Hwang <iambigman97@naver.com>
2023-11-24 21:29:23 +0900

    Refactor: 주문 요청 DTO 변경
    주문 요청 DTO 변경
    
    Related to: #66

---
+++ src/main/java/com/example/miniproject/domain/order/dto/request/ClientRequest.java
@@ -0,0 +1,14 @@
+package com.example.miniproject.domain.order.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+
+public record ClientRequest(
+
+    @NotBlank
+    String name,
+
+    @NotBlank
+    String phoneNumber
+) {
+
+}
