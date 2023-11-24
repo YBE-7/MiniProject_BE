5e0a73110c06fb14f92e3c41a647a5d81a574040
KyuCheol Hwang <iambigman97@naver.com>
2023-11-24 17:41:21 +0900

    Refactor: 객실 타입 조회 메소드명 변경
    객실 타입 조회 메소드명 search로 변경
    
    Related to: #58

--- src/main/java/com/example/miniproject/domain/roomtype/service/RoomTypeService.java
+++ src/main/java/com/example/miniproject/domain/roomtype/service/RoomTypeService.java
@@ -14,7 +14,7 @@ import java.util.Optional;
 public interface RoomTypeService {
     RoomTypesRegisterResponse register(RoomTypesRegisterRequest request);
 
-    List<RoomTypeResponse> getRoomTypes(
+    List<RoomTypeResponse> search(
         Long accommodationId,
         RoomTypeSearchCondition condition
     );
