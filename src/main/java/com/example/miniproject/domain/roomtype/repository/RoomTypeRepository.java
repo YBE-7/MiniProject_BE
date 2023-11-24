4268d64e2c2e5ba0b578357e6374df311ddbde90
KyuCheol Hwang <iambigman97@naver.com>
2023-11-24 17:40:51 +0900

    Refactor: 객실 타입 조회 Repository 로직 변경
    객실 타입 조회 Repository로직 쿼리 하나로 해결
    
    Related to: #58

--- src/main/java/com/example/miniproject/domain/roomtype/repository/RoomTypeRepository.java
+++ src/main/java/com/example/miniproject/domain/roomtype/repository/RoomTypeRepository.java
@@ -1,13 +1,9 @@
 package com.example.miniproject.domain.roomtype.repository;
 
-import com.example.miniproject.domain.accommodation.entity.Accommodation;
 import com.example.miniproject.domain.roomtype.entity.RoomType;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface RoomTypeRepository
     extends JpaRepository<RoomType, Long>, RoomTypeRepositoryCustom {
-
-    List<RoomType> findByAccommodationAndCapacityGreaterThanEqual(Accommodation accommodation, Integer capacity);
 
 }
