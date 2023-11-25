63f2185eb57c15094266a59acdeb517f47a6d658
KyuCheol Hwang <iambigman97@naver.com>
2023-11-25 23:37:47 +0900

    Refactor: 주문시 예약자, 이용자 validation 추가
    예약자, 이용자 이름과 전화번호 validation 기능 추가
    
    Related to: #82

--- src/main/java/com/example/miniproject/domain/order/dto/request/ClientRequest.java
+++ src/main/java/com/example/miniproject/domain/order/dto/request/ClientRequest.java
@@ -1,13 +1,16 @@
 package com.example.miniproject.domain.order.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 
 public record ClientRequest(
 
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z가-힣]{2,16}$")
     String name,
 
     @NotBlank
+    @Pattern(regexp = "^([0-9]{3})-([0-9]{3,4})-([0-9]{4})$")
     String phoneNumber
 ) {
 
