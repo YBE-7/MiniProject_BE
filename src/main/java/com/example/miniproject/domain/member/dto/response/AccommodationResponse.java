914a9cd49fefd69e8bb43412142cd7cb13ff3372
KyuCheol Hwang <iambigman97@naver.com>
2023-11-27 16:05:21 +0900

    Refactor: 주문 내역 조회 응답 DTO 변경
    주문 내역 조회 응답 DTO에 숙소 타입 추가
    
    Related to: #93

--- src/main/java/com/example/miniproject/domain/member/dto/response/AccommodationResponse.java
+++ src/main/java/com/example/miniproject/domain/member/dto/response/AccommodationResponse.java
@@ -1,15 +1,18 @@
 package com.example.miniproject.domain.member.dto.response;
 
 import com.example.miniproject.domain.accommodation.entity.Accommodation;
+import com.example.miniproject.global.constant.AccommodationType;
 
 public record AccommodationResponse(
     Long id,
+    AccommodationType type,
     String name,
     String image
 ) {
     public AccommodationResponse(Accommodation accommodation) {
         this(
             accommodation.getId(),
+            accommodation.getType(),
             accommodation.getName(),
             accommodation.getThumbnailUrl()
         );
