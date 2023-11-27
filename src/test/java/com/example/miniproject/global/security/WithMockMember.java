c140d46868671b39bd94ec77748afdca8515c090
KyuCheol Hwang <iambigman97@naver.com>
2023-11-27 16:22:26 +0900

    Feat: Test용 Mock 사용자 어노테이션 구현
    Test용 Mock 사용자 어노테이션 구현
    
    Related to: #94

---
+++ src/test/java/com/example/miniproject/global/security/WithMockMember.java
@@ -0,0 +1,12 @@
+package com.example.miniproject.global.security;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import org.springframework.security.test.context.support.WithSecurityContext;
+
+@Retention(RetentionPolicy.RUNTIME)
+@WithSecurityContext(factory = WithMockMemberSecurityContextFactory.class)
+public @interface WithMockMember {
+    long id() default 1L;
+    String[] authorities() default "ROLE_MEMBER";
+}
