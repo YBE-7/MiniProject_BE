1bbdd49f0730fa40a71104a83d4bdf7f037e14e6
rmaWhrdl <wwscan3@gmail.com>
2023-12-06 14:32:53 +0900

    Refactor: OrderServiceFacade 코드 수정
    가독성 향상을 위한 OrderServiceFacade 코드 수정
    
    Related to: #112

--- src/main/java/com/example/miniproject/domain/order/service/OrderServiceFacade.java
+++ src/main/java/com/example/miniproject/domain/order/service/OrderServiceFacade.java
@@ -15,7 +15,7 @@ import java.util.concurrent.TimeUnit;
 public class OrderServiceFacade {
 
     private static final int LOCK_WAIT_TIME = 3;
-    private static final int LOCK_LEASE_TIME = 1;
+    private static final int LOCK_LEASE_TIME = 5;
     private static final String LOCK_NAME_PREFIX = "order_lock_";
     private static final String LOCK_EXCEPTION_MESSAGE = "Lock을 획득하지 못했습니다.";
     private static final String INTERRUPTED_EXCEPTION_MESSAGE = "Lock을 획득하는 동안 인터럽트가 발생했습니다.";
@@ -27,22 +27,25 @@ public class OrderServiceFacade {
         RLock[] locks = request.orderItems()
             .stream()
             .map(OrderItemRegisterRequest::roomTypeId)
-            .map(Object::toString)
+            .distinct()
             .map(key -> redissonClient.getLock(LOCK_NAME_PREFIX + key))
             .toArray(RLock[]::new);
 
+        RLock multiLock = redissonClient.getMultiLock(locks);
+
         try {
-            if (redissonClient.getMultiLock(locks).tryLock(LOCK_WAIT_TIME, LOCK_LEASE_TIME, TimeUnit.SECONDS)) {
-                try {
-                    return orderService.registerOrder(memberId, request);
-                } finally {
-                    redissonClient.getMultiLock(locks).unlock();
-                }
-            } else {
+            if (!multiLock.tryLock(
+                LOCK_WAIT_TIME,
+                LOCK_LEASE_TIME,
+                TimeUnit.SECONDS
+            )) {
                 throw new RuntimeException(LOCK_EXCEPTION_MESSAGE);
             }
+            return orderService.registerOrder(memberId, request);
         } catch (InterruptedException ex) {
             throw new RuntimeException(INTERRUPTED_EXCEPTION_MESSAGE, ex);
+        } finally {
+            multiLock.unlock();
         }
     }
 }
